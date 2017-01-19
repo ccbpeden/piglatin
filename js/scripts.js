5577cd4a9bda32885d1e83be71518e0b85c0f860
Charles Peden and Calvin Cushman <calvin@>
2017-01-18 16:15:16 -0800

    add vowel logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
-var trigraph = ['que','squ','str','str','thr','spr','sph','shr','scr','sch']
+var initClusters = ["trigraph", "digraph", "consonant", "vowel"]
 
-var digraph = ['bl','br','ch','cl','cr','dr','fl','fr','gl','gr','pl','pr','sc','sh','sk','sl','sm','sn','sp','st','sw','th','tr','tw','wh','wr']
+var clusterCounter = ["2", "1", "0", "0"]
 
+var trigraph = ['squ','str','str','thr','spr','sph','shr','scr','sch']
+
+var digraph = ['bl','br','ch','cl','cr','dr','fl','fr','gl','gr','pl','pr','sc','sh','sk','sl','sm','sn','sp','st','sw','th','tr','tw','wh','wr','qu']
+
+var vowels = ["a", "e", "i", "o", "u", "y"]
+
+var consonants = ["b","c","d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
 var finalPunct
 
 var stringSplitter = function(inputString) {
@@ -19,16 +26,37 @@ var punctTest = function(splitStrings){
   };
 };
 
-var trigraphTest = function(punctTest){
+var trigraphTest = function(splitStrings){
+ for(var wordIndex = 0, wordIndex<splitStrings.length, WordIndex+=1){
+   for(var clusterIndex = 0, initSound!=null, clusterIndex+=1){
+     var initCluster = splitStrings[wordIndex].slice(0,[clusterIndex])
 
-};
+//      if initClust
+//    }
+//  }
+//  var initSound = splitStrings[index]
+
+//  var trigraph = splitStrings[0].slice(0,2)
+//  console.log("trigraph is equal to " + trigraph)
+// };
 
 var digraphTest = function(punctTest){
 
 }
 
-var vowelTest = function(punctTest){
+var vowelTest = function(splitStrings){
+  var firstChar = splitStrings[0].slice(0);
+  for(var index = 0; index < vowels.length; index+=1)
+  if (firstChar = vowels[index]){
+    splitStrings[index] = splitStrings[index] + "way";
+    return splitStrings[index];
+  } else {
+    var firstChar = splitStrings[0].slice(0);
+    for(var index = 0; index < consonants.length; index+=1)
+    if (firstChar = consonants[index]){
 
+
+  }
 }
 
 var consonantTest = function(punctTest){
@@ -49,8 +77,9 @@ $(document).ready(function() {
 //      console.log('inputString set to ' + inputString);
       var splitStrings = stringSplitter(inputString);
 //      console.log('splitStrings set to ' + splitStrings);
-      finalPunct = punctTest(splitStrings);
-      console.log(finalPunct);
+//      finalPunct = punctTest(splitStrings);
+      var result = vowelTest(splitStrings);
+      console.log(result);
       $("#result").text(result);
     } else alert('please enter text');
   });
